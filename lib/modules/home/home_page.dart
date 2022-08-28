3aab9cbf39ce5c25fa173d967418c64f5ee01014
Walex Souza <walex.souza.santos@gmail.com>
2022-08-28 18:44:55 -0300

    Feat: Add logo in home_page.dart

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:valorant/shared/app_colors.dart';
+import 'package:valorant/shared/app_images.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +14,24 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            AppImages.logo,
+            height: 100,
+          ),
+          const Text(
+            'Choose your agent',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
